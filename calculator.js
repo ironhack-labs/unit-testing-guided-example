9b72e0e8dd43796a8eda29fe4b7a9706529041b2
José Carneiro <contact@josecarnei.ro>
2021-04-09 16:30:08 +0100

    Finilizes solution

--- calculator.js
+++ calculator.js
@@ -1,13 +1,14 @@
-function sum(a, b) {
-  return;
+function sum(a = 0, b = 0) {
+  return a + b;
 }
 
-function subtract(a, b) {
-  return;
+function subtract(a = 0, b = 0) {
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) throw new Error("Cannot divide by 0");
+  return a / b;
 }
 
 function multiply(a, b) {
