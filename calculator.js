7acd661d6c449864ce152bee71c36f8c4d825cf3
ross-u <cirkovic.uros.srb@gmail.com>
2021-05-27 14:53:34 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,18 +1,19 @@
 function sum(a, b) {
-  return a + b;
+  a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  a - b;
 }
 
 function divide(a, b) {
-  return a / b;
+  a / b;
 }
 
 function multiply(a, b) {
   return a * b;
 }
 
-// The following is required to make unit tests work. Please ignore it.
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
 module.exports = { sum, subtract, divide, multiply };
