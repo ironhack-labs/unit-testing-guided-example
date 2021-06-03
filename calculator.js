df7c449a3234e0297b2a48bc8ef52f159b76ce3b
Alexander Marino <amarinoglobal@gmail.com>
2021-06-03 16:46:24 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,52 @@
 function sum(a, b) {
-  return;
+
+  if (a == undefined && b == undefined) {
+    result = 0
+  }
+  else if (a == undefined) {
+
+    result = b + 0
+  }
+
+  else if (b == undefined) {
+
+    result = a + 0
+  }
+  else {
+    result = a + b
+  }
+
+  return result;
 }
 
 function subtract(a, b) {
-  return;
+  if (a == undefined && b == undefined) {
+    result = 0
+  }
+  else if (a == undefined) {
+
+    result = b - 0
+  }
+
+  else if (b == undefined) {
+
+    result = a - 0
+  }
+
+  else {
+    result = a - b
+  }
+  return result;
 }
 
 function divide(a, b) {
-  return;
+
+  if (a && b) { result = a / b }
+  else if (b == 0) { throw "" }
+
+  return result;
+
+
 }
 
 function multiply(a, b) {
