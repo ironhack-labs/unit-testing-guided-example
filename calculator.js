f6878aaf503b23388b1a3a8ccba79b00872d8be2
kevinu27 <kevin.sm89@gmail.com>
2021-06-03 16:56:58 +0100

    Testing solved

--- calculator.js
+++ calculator.js
@@ -1,13 +1,37 @@
 function sum(a, b) {
-  return;
+  if ( a === undefined && b === undefined){
+      return 0
+  }
+  if ( b === undefined){
+      return a + 0
+  }
+   if ( a === undefined){
+      return b + 0
+  }
+
+   return a +b;
 }
 
 function subtract(a, b) {
-  return;
+if ( a === undefined && b === undefined){
+      return 0
+  }
+  if ( b === undefined){
+      return a - 0
+  }
+   if ( a === undefined){
+      return b - 0
+  }
+
+
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+    if ( b ==0){
+      return error;
+  }
+  return a/b;
 }
 
 function multiply(a, b) {
