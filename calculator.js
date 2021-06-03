6393bf1c15537ce4acb415299b3bed74c640f3fe
MATS <waanmedia_mats@MacBook-Pro-de-WaanMedia-MATS.local>
2021-06-03 23:07:18 +0200

    ok

--- calculator.js
+++ calculator.js
@@ -1,17 +1,44 @@
+
+
 function sum(a, b) {
-  return;
+  if (a == undefined && b == undefined)
+    return (0);
+  if (a == undefined)
+    return (b);
+  if (b == undefined)
+    return (a);
+
+
+  return (a + b);
+
+
+
 }
 
 function subtract(a, b) {
-  return;
+
+  if (a == undefined && b == undefined)
+    return (0);
+
+  if (a == undefined)
+    return (b);
+  if (b == undefined)
+    return (a);
+
+  return (a - b);
 }
 
 function divide(a, b) {
-  return;
+
+  if (b == 0)
+    throw new Error('You can not divide by zero');
+
+  return (a / b);
 }
 
+
 function multiply(a, b) {
-  return a * b;
+  return (a * b);
 }
 
 // The following is required to make unit tests work. Please ignore it.
