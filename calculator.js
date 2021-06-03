1e5dec3c7d00f0ce8f8b899fdb93aff8c934c244
Samuel Gallego <samuel.gallego15@gmail.com>
2021-06-03 19:27:29 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,18 +1,44 @@
 function sum(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0
+  }
+  else if (a === undefined || b === undefined) {
+    return a + 0
+  }
+  else {
+    return a + b
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0
+  }
+  else if (a === undefined || b === undefined) {
+    return a - 0
+  }
+  else {
+    return a - b
+  }
+
 }
 
 function divide(a, b) {
-  return;
+  let splitTwoNumbers = 0
+  if (a === 0 || b === 0) {
+    throw new Error('')
+  }
+  else {
+    splitTwoNumbers = a / b
+  }
+  return splitTwoNumbers
+
 }
 
 function multiply(a, b) {
-  return a * b;
+  return;
 }
+
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
