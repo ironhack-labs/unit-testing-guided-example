d20493df73aac2bcebce7dfe8c6fe9b2df71156b
Konhin CM <maskmesta@gmail.com>
2021-06-22 11:38:03 -0500

    done

--- calculator.js
+++ calculator.js
@@ -1,17 +1,40 @@
-function sum(a, b) {
-  return;
-}
+console.log("Hola")
 
+function sum(a, b) {
+  if (a === undefined && b === undefined) {
+    return 0
+  } else if (a === undefined || b === undefined) {
+    if (a === undefined) {
+      return b + 0
+    } else {
+      return a + 0
+    }
+  } else {
+    return a + b
+  }
+}
 function subtract(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0
+  } else if (a === undefined || b === undefined) {
+    if (a === undefined) {
+      return b - 0
+    } else {
+      return a - 0
+    }
+  }
+  return a - b
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0){
+    throw Error("You can't divided by 0")
+  }
+  return a / b
 }
 
 function multiply(a, b) {
-  return a * b;
+  return a * b
 }
 
 // The following is required to make unit tests work. Please ignore it.
