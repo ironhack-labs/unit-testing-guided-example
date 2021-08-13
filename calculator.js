51f18e426ac7e56d39f3929bd83edb910cf08b03
Ricardo Zurita <hola@ricardo-zurita.com>
2021-08-13 07:40:20 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,34 @@
 function sum(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    a = 0;
+    b = 0;
+  }
+  if (a === undefined) {
+    a = 0;
+  } else if (b === undefined) {
+    b = 0;
+  }
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    a = 0;
+    b = 0;
+  }
+  if (a === undefined) {
+    a = 0;
+  } else if (b === undefined) {
+    b = 0;
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw new Error('Cannot divide by 0');
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
