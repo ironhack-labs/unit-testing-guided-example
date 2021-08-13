9ad3619d0a5eed7720ea82dffb7c3b04ef26f36c
jose ramon <rabohopo@gmail.com>
2021-08-13 09:50:00 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,39 @@
 function sum(a, b) {
-  return;
-}
+ 
+ if(a == undefined && b == undefined){
+  result = 0; 
+  return result;
+ }if(b == undefined){ 
+  return a;
+   }else { 
+  result = a + b
+  return result;
+   }
+ }
+
 
 function subtract(a, b) {
-  return;
-}
+  if(a == undefined && b == undefined){
+    result = 0; 
+    return result;
+   }if(b == undefined){ 
+    return a;
+     }else { 
+       result = a -  b
+        return result;
+     }
+   }
+  
+
 
 function divide(a, b) {
-  return;
+if(b == 0){
+ result = error;
+ return result;
+}else{ 
+  result = a/b
+  return result;
+ }
 }
 
 function multiply(a, b) {
