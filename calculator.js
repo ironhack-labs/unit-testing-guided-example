755cd3bfcca5c82e487c1ebf0f37d9d34ecfd453
AlexVonrez <alexvonrez1@gmail.com>
2021-10-21 18:00:59 +0200

    testing calculator done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,31 @@
 function sum(a, b) {
-  return;
+if (!a && !b ){
+  return 0;
+}else if(!a || !b){
+     return a;
+   }else{
+   return a + b;
+   }
+
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b){
+    return 0;
+  }else if (!a || !b){
+     return a;
+  }else{
+  
+  return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {return error}
+  else {
+    return a/ b
+  }
+
 }
 
 function multiply(a, b) {
