160c5bdb444b66ee7d33a160ecd5c125a441f9c3
besaga <bsgallardo93@gmail.com>
2021-10-21 21:23:09 +0200

    final_calculadora

--- calculator.js
+++ calculator.js
@@ -1,18 +1,54 @@
 function sum(a, b) {
-  return;
+
+  if (!a && !b) {
+    return 0
+  }
+  if (!a && !b) {
+    return 0
+  }
+  if (!a) {
+    return b + 0
+  } if (!b) {
+    return a + 0
+  }
+
+
+  return (a + b);
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    return 0
+  }
+  if (!a) {
+    return b - 0
+  } if (!b) {
+    return a - 0
+  }
+  return (a - b);
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw error
+  }
+
+
+  return (a / b);
+
 }
 
+
+
+
 function multiply(a, b) {
-  return a * b;
+  if (!a) {
+    return b * 0
+  }
+  return (a * b);
 }
+
+
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
--- tests/sum.spec.js
+++ tests/sum.spec.js
@@ -6,20 +6,31 @@ describe("Sum", () => {
     expect(result).toBe(3);
   });
 
+
+
+
   test("should add up two floating point numbers correctly", () => {
     const result = sum(1.35, 2.29);
     expect(result).toBe(3.64);
   });
+
+
 
   test("should add up a negative and a positive number correctly", () => {
     const result = sum(-1.35, 2.29);
     expect(result).toBe(0.94);
   });
 
+
+
+
   test("should return 0 when function is called without arguments", () => {
     const result = sum();
     expect(result).toBe(0);
   });
+
+
+  
 
   test("should sum a value with 0 when function is called with a single argument", () => {
     const result = sum(26);
