e61d561fc010f5ed25d8ff07a0d38512ffa2c2e9
mmonereo <mauro.monereo@gmail.com>
2021-10-21 17:16:31 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,17 +1,45 @@
 function sum(a, b) {
-  return;
+
+	if (!a && !b) {
+		return 0
+	}
+	if (!a) {
+		return b + 0;
+	}
+	if (!b) {
+		return a + 0;
+	}
+
+	return a + b;
 }
 
 function subtract(a, b) {
-  return;
+	if (!a && !b) {
+		return 0
+	}
+	if (!a) {
+		return b - 0;
+	}
+	if (!b) {
+		return a - 0;
+	}
+
+	return a - b;
 }
 
 function divide(a, b) {
-  return;
+	if (!a && !b) {
+		return 0
+	}
+	if (b === 0) {
+		throw new Error('Dividing by zero is not possible');
+	}
+
+	return a / b;
 }
 
 function multiply(a, b) {
-  return a * b;
+	return a * b;
 }
 
 // The following is required to make unit tests work. Please ignore it.
