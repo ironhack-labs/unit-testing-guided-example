a976ef915a2cfd761ba4a5156a64ed445c20a973
Iñigo <inigo@MacBook-Air-de-Inigo.local>
2021-10-21 18:52:31 +0200

    ejercicio listo

--- calculator.js
+++ calculator.js
@@ -1,14 +1,48 @@
-function sum(a, b) {
-  return;
+function sum(a, b){
+
+if (a === undefined && b === undefined) {
+  return 0
+}
+
+//SI b no es un valor
+//retornar a + 0
+
+if (b=== undefined) {
+  return (a+0) 
+
+
+
+}
+
+ return (a+b)
+
 }
 
 function subtract(a, b) {
-  return;
+  if (!a&&!b)  {
+return 0 
+  }
+  
+a = a || 0
+b = b || 0
+
+
+return a-b;
+
+
 }
 
 function divide(a, b) {
-  return;
+  if (b==0){
+  throw "error";
+  }
+  return a/b;
+
 }
+
+
+ 
+
 
 function multiply(a, b) {
   return a * b;
@@ -16,3 +50,6 @@ function multiply(a, b) {
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
+
+
+
