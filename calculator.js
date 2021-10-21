3723319cf2804ab370334238a804aee4541a2329
Carlos Jiménez <charly.design.c@gmail.com>
2021-10-21 17:53:01 +0200

    calculator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,45 @@
 function sum(a, b) {
-  return;
+  if (a === undefined) {
+    return 0;
+  }
+
+  if (!a && b) { 
+    return b+0;
+  } else if (a && !b){
+    return a+0;
+  }
+
+  return a+b;
+  
+  
 }
 
+
 function subtract(a, b) {
-  return;
+  
+
+  if (!a) {
+    return 0;
+  }
+
+  if (!a && b) {
+    return b-0;
+  } else if (a && !b){
+    return a-0;
+  }
+
+  return a-b;
+  
 }
 
 function divide(a, b) {
-  return;
+  
+  
+  if (b === 0) {
+    throw "Error"
+  }
+
+  return a/b;
 }
 
 function multiply(a, b) {
@@ -16,3 +48,5 @@ function multiply(a, b) {
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
+
+
