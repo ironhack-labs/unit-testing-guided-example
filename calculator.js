df8144257cb0b74b0f18c3cbdb1d7f0210d5c5a2
marferbl <fernandezblascomarcos@gmail.com>
2021-10-21 17:05:32 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,12 +1,32 @@
 function sum(a, b) {
+  if (!a && !b) {
+    return 0;
+  }
+  else if (!a) { return 0 + b }
+  else if (!b) { return 0 + a }
+  else return a + b;
+
+
+
+
+
+
   return;
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  }
+  else if (!a) { return b - 0 }
+  else if (!b) { return a - 0 }
+
+  return a - b;
 }
 
 function divide(a, b) {
+  if (b == 0) { throw "Error2"; }
+  else return a / b;
   return;
 }
 
