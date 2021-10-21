2dd204330b226df99720aaed287f9534a6e6e31d
Lisa Medina Gutiérrez <lisa.medinag@gmail.com>
2021-10-21 18:23:20 +0200

    FirstSubmission

--- calculator.js
+++ calculator.js
@@ -1,18 +1,63 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  }
+  else if(!a)
+  {
+    return b
+  }
+  else if (!b){
+    return a
+  }
+  else{
+    let result = a + b;
+    return result
+  }
 }
 
 function subtract(a, b) {
-  return;
+
+  if (!a && !b) {
+    return 0;
+  }
+  else if(!a)
+  {
+    return b
+  }
+  else if (!b){
+    return a
+  }
+  else{
+    let result = a - b;
+    return result
+  }
 }
 
 function divide(a, b) {
-  return;
+
+  if (a===0 || b===0)
+  {
+    throw "Error";
+  }
+
+  else{
+    let result = a / b;
+    return result
+  }
+
+  const result = a-b
+  return result;
 }
 
 function multiply(a, b) {
-  return a * b;
+  const result = a*b
+  return result;
 }
+
+
+
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
+
+
