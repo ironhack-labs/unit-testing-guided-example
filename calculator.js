08d90a3e82b97d71c6d5b1c3df806ccbb79d9e1e
SilviuDN <silviudilimot@gmail.com>
2022-01-13 22:32:01 +0100

    one solution

--- calculator.js
+++ calculator.js
@@ -1,17 +1,45 @@
 function sum(a, b) {
-  return;
+  if( a === undefined ){
+    return 0
+  }
+  if( b === undefined ){
+    return a
+  }
+  // if ( exception(a, b) !== NaN ){
+  //   return exception(a, b)
+  // }
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if( a === undefined ){
+    return 0
+  }
+  if( b === undefined ){
+    return a
+  }
+  return a-b;
 }
 
 function divide(a, b) {
-  return;
+  if (b == 0){
+    throw("Error!")
+  }
+  return a/b;
 }
 
 function multiply(a, b) {
   return a * b;
+}
+
+function exception(a, b){
+  if( a === undefined ){
+    return 0
+  }
+  if( b === undefined ){
+    return a
+  }
+  return NaN;
 }
 
 // The following is required to make unit tests work. Please ignore it.
