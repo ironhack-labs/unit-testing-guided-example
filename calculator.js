91f2e17f45ddb25059f0cdf9d0b08bff0329cae2
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-03-28 22:06:46 -0600

    Lab complete

--- calculator.js
+++ calculator.js
@@ -1,13 +1,32 @@
 function sum(a, b) {
-  return;
+  if (!b){
+    b = 0;
+  }
+
+  if (!a && !b){
+    a = 0;
+    b = 0;
+  }
+  return a +b;
 }
 
 function subtract(a, b) {
-  return;
+  if (!b){
+    b = 0;
+  }
+
+  if (!a && !b){
+    a = 0;
+    b = 0;
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b===0){
+    throw "Can't divide by 0";
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
