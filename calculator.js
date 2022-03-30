62449c4bffe1bf26aba1c0ad6b8150d12f84f086
Ha Thu <tranhathu6989@gmail.com>
2022-03-30 03:16:34 +0200

    Solved lab

--- calculator.js
+++ calculator.js
@@ -1,13 +1,27 @@
-function sum(a, b) {
-  return;
+function sum(a = 0, b = 0) {
+  return a + b
 }
 
 function subtract(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0
+  } else if (a === undefined || b === undefined) {
+    return a - 0
+  } else if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a - b
+  } else if (!Number.isInteger(a) && !Number.isInteger(b)) {
+    return parseFloat(a - b)
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (a != 0 && b === 0) {
+    throw new Error('Test Error')
+  } else if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a / b
+  } else if (!Number.isInteger(a) && !Number.isInteger(b)) {
+    return parseFloat(a / b)
+  }
 }
 
 function multiply(a, b) {
