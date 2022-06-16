9dc0ea66f8485cdd83ae4cb2096c6e91feaa1263
Palash <chadhapalash2000@gmail.com>
2022-06-16 13:03:16 +0400

    unit testing by Palash

--- calculator.js
+++ calculator.js
@@ -1,13 +1,29 @@
 function sum(a, b) {
-  return;
+  if (b === undefined && a === undefined) {
+    return 0;
+  } else if (b === undefined) {
+    return a;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (b === undefined && a === undefined) {
+    return 0;
+  } else if (b === undefined) {
+    return a;
+  } else {
+    return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw new Error('cannot divide by 0 idiot');
+  } else {
+    return a / b;
+  }
 }
 
 function multiply(a, b) {
