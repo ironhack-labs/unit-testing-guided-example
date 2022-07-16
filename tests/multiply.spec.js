139f282d5f0d07e8a7ebddd4add5cda5478237cd
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-07-16 13:07:35 -0300

    100% de sucesso nos testes!

--- tests/multiply.spec.js
+++ tests/multiply.spec.js
@@ -1,31 +1,18 @@
 const { multiply } = require('./../calculator.js');
 
-
-
-
 describe("Multiply", () => {
   test("The value returned should be the result of the multiplication of both values.", () => {
     const result = multiply(1, 2);
-    expect(result).toBe(3);
+    expect(result).toBe(2);
   });
 
   test("The value returned should be a negative number.", () => {
-    const result = multiply(1.35, 2.29);
-    expect(result).toBe(3.64);
+    const result = multiply(2, -3);
+    expect(result).toBe(-6);
   });
 
   test("The value returned should be 0 (any number multiplied by 0 equals 0).", () => {
-    const result = multiply(-1.35, 2.29);
-    expect(result).toBe(0.94);
-  });
-
-  test("should return 0 when function is called without arguments", () => {
-    const result = multiply();
+    const result = multiply(10, 0);
     expect(result).toBe(0);
-  });
-
-  test("should multiply a value with 0 when function is called with a single argument", () => {
-    const result = multiply(26);
-    expect(result).toBe(26);
   });
 });
