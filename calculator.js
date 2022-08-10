c39f31661209d4173749667eee394af96a9ed660
Stefano Silva <stefanodavi1sx@gmail.com>
2022-08-10 18:33:47 -0300

    done until iteration 7

--- calculator.js
+++ calculator.js
@@ -1,16 +1,35 @@
 function sum(a, b) {
-  return;
+  if (b === undefined) {
+    b = 0;
+  }
+  if (a === undefined) {
+    a = 0;
+  }
+
+  return a + b;
 }
 
+
 function subtract(a, b) {
-  return;
+  if (b === undefined) {
+    b = 0;
+  }
+  if (a === undefined) {
+    a = 0;
+  }
+
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw new Error('Cannot divide by 0');
+  }
+  return a/b;
 }
 
 function multiply(a, b) {
+  
   return a * b;
 }
 
